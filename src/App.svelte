e13744e2c0bf84a9bc3ef7735fd20d0f87db9175
gagan <dp.gagn@gmail.com>
2019-08-07 10:07:42 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = "world";
+</script>
+
+<style lang="scss`">
+  h1 {
+    color: pink;
+    span {
+      color: red;
+    }
+  }
+</style>
+
+<h1>
+  Hello
+  <span>{name}!</span>
+</h1>
